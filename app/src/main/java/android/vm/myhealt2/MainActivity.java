0da072a101c44d252aa79a25b087561710be1bb6
FrancescoCamposeo <camposeofrancesco1@gmail.com>
2017-01-27 09:29:40 +0100

    First activity

---
+++ app/src/main/java/android/vm/myhealt2/MainActivity.java
@@ -0,0 +1,41 @@
+package android.vm.myhealt2;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+    EditText medical, date;
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+    int clickCounter=0;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.content_main);
+        medical = (EditText) findViewById(R.id.medical_name);
+        date = (EditText) findViewById(R.id.date);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Nome medicinale : "+ medical.getText() + "\n Data : " + date.getText() );
+        medical.setText("");
+        date.setText("");
+        adapter.notifyDataSetChanged();
+    }
+}
