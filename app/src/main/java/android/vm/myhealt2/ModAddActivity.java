c499cdaa92e4429588057abc375a2e8ac989053f
FrancescoCamposeo <camposeofrancesco1@gmail.com>
2017-02-01 13:03:45 +0100

    add camera

--- app/src/main/java/android/vm/myhealt2/ModAddActivity.java
+++ app/src/main/java/android/vm/myhealt2/ModAddActivity.java
@@ -1,9 +1,13 @@
 package android.vm.myhealt2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -12,11 +16,21 @@ import android.widget.EditText;
 
 public class ModAddActivity extends AppCompatActivity {
 
+    Button scanner;
+    Activity activity = this;
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
        setContentView(R.layout.activity_mod_add);
+        scanner = (Button) findViewById(R.id.bottone_scanner);
 
+        scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(activity,ScanProdottoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/android/vm/myhealt2/ScanProdottoActivity.java
@@ -0,0 +1,64 @@
+package android.vm.myhealt2;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.content.ContentValues.TAG;
+
+public class ScanProdottoActivity extends Activity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(this, "Contents = " + result.getText() +
+
+                ", Format = " + result.getBarcodeFormat().toString(), Toast.LENGTH_SHORT).show();
+        //Do something with result here
+
+        Log.v(TAG, result.getText());
+        Log.v(TAG, result.getBarcodeFormat().toString());
+
+        //If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+    }
+}
